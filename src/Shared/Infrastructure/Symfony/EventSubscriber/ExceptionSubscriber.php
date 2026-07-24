78ee1824519034197b8a4b10049143d03c339bd6
Gwennael Jean <gwennael.jean@linkt.fr>
2026-07-24 17:21:11 +0200

    feat(auth): gère le mapping des exceptions

---
+++ src/Shared/Infrastructure/Symfony/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Shared\Infrastructure\Symfony\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        #[Autowire('%exception_mapper%')]
+        private readonly array $mapping,
+    )
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onException'
+        ];
+    }
+
+    public function onException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $message = $exception->getMessage();
+
+        foreach ($this->mapping as $class => $mapping) {
+            if ($exception instanceof $class) {
+                $event->setResponse(new JsonResponse([
+                    'error' => $mapping['error'],
+                    'message' => !!$message ? $message : null,
+                ], $mapping['status']));
+            }
+        }
+    }
+}
